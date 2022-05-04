b2fa1e3f47f46191d4a05d8d93c5b734f432dff6
주니엘 <howon5@naver.com>
2022-05-04 16:30:10 +0900

    2022-05-04
    TurnCard create

---
+++ WASD_Unity/Assets/Script/TurnCard.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnCard : MonoBehaviour
+{
+    public Sprite spriteCardFront;
+    public Sprite spriteCardBack;
+
+    static bool isFront = true; 
+    static float speed = 4.0f;
+
+    static bool choice = false;
+
+    RectTransform rectTransform;
+    Image cardImage;
+
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        cardImage = GetComponent<Image>();
+    }
+
+    private void Start()
+    {
+        //spriteCardFront = GetComponent<Sprite>();
+
+        if (isFront)
+        {
+            cardImage.sprite = spriteCardFront;
+        }
+        
+        else
+        {
+            cardImage.sprite = spriteCardBack;
+        }
+        
+    }
+
+    public void StartTurn()
+    {
+       
+            StartCoroutine(Turn());
+    }
+
+    IEnumerator Turn()
+    {
+        float tick = 0f;
+
+        Vector3 startScale = new Vector3(1.0f, 1.0f, 1.0f);
+        Vector3 endScale = new Vector3(0f, 1.0f, 1.0f);
+
+        Vector3 localScale = new Vector3();
+
+
+        while (tick < 1.0f)
+        {
+            tick += Time.deltaTime * speed;
+
+            localScale = Vector3.Lerp(startScale, endScale, tick);
+
+            rectTransform.localScale = localScale;
+
+            yield return null;
+        }
+
+        if (choice == false)
+        {
+            cardImage.sprite = spriteCardBack;
+            choice = true;
+        }
+        isFront = !isFront;
+
+        tick = 0f;
+
+        while (tick < 1.0f)
+        {
+            tick += Time.deltaTime * speed;
+
+            localScale = Vector3.Lerp(endScale, startScale, tick);
+
+            rectTransform.localScale = localScale;
+
+            yield return null;
+        }
+    }
+}
