31f71cf7973305cb5523b7857d7357c6844b8cfe
KYJ <xpfjsdkdlel@naver.com>
2022-06-01 16:03:22 +0900

    06.01
    체력 오류 수정

--- WASD_Unity/Assets/Scripts/Hearts.cs
+++ WASD_Unity/Assets/Scripts/Hearts.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class Hearts : MonoBehaviour
 {
     private List<Animation> hearts = new List<Animation>();
+    private GameObject heart;
     // Start is called before the first frame update
     public void Init()
     {
+        for(int i = 1; i <= 5; ++i)
+        {
+            heart = GameObject.Find(i.ToString());
+            if (i > GameData.HP)
+                Destroy(heart);
+        }
         for(int i = 0; i < transform.childCount; ++i)
         {
             Transform t = transform.GetChild(i);
